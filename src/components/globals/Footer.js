c3919c32518cc017d683a58d4c48d8ee829000ac
Clive Noonan <clivenoonan93@gmail.com>
2019-04-20 22:00:03 +0100

    bug in menu

--- src/components/globals/Footer.js
+++ src/components/globals/Footer.js
@@ -10,6 +10,16 @@ export default class Footer extends Component {
         icon: <FaFacebook className="icon facebook-icon" />,
         path: `https://www.facebook.com`,
       },
+      {
+        id: 2,
+        icon: <FaTwitter className="icon twitter-icon" />,
+        path: `https://www.facebook.com`,
+      },
+      {
+        id: 3,
+        icon: <FaInstagram className="icon instagram-icon" />,
+        path: `https://www.facebook.com`,
+      },
     ],
   }
   render() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
-import * as styles from './styles'
-import { HomeHeader, PageHeader } from './Header'
-import { Banner } from './Banner'
-import { BannerButton, SectionButton } from './Button'
-import { Section } from './Section'
-import { Title } from './Title'
-export {
-  styles,
-  HomeHeader,
-  Banner,
-  BannerButton,
-  PageHeader,
-  Section,
-  Title,
-  SectionButton,
-}
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { HomeHeader, Banner, BannerButton } from '../utils'
+import img from '../images/bcg/homeBcg.jpeg'
+import QuickInfo from '../components/HomePageComponents/QuickInfo'
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <HomeHeader img={img}>
+      <Banner title="Tony's" subtitle=" main street, carrigaline, cork, ireland ">
+        <Link to="/menu/" style={{ textDecoration: 'none' }}>
+          <BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
+        </Link>
+      </Banner>
+    </HomeHeader>
+    <QuickInfo />
+  </Layout>
+)
+
+export default IndexPage
