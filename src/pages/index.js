0d4222da2377161c508950be8fc9edfff6b0f8a3
Clive Noonan <clivenoonan93@gmail.com>
2019-04-20 21:25:50 +0100

    Added site wide footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as styles from './styles'
 import { HomeHeader, PageHeader } from './Header'
 import { Banner } from './Banner'
@@ -15,29 +14,3 @@ export {
   Title,
   SectionButton,
 }
-=======
-import React from 'react'
-import { Link } from 'gatsby'
-
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import { HomeHeader, Banner, BannerButton } from '../utils'
-import img from '../images/bcg/homeBcg.jpeg'
-import QuickInfo from '../components/HomePageComponents/QuickInfo'
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <HomeHeader img={img}>
-      <Banner title="tony's" subtitle="Main street - Carrigaline, Co Cork Ireland">
-        <Link to="/menu/" style={{ textDecoration: 'none' }}>
-          <BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
-        </Link>
-      </Banner>
-    </HomeHeader>
-    <QuickInfo />
-  </Layout>
-)
-
-export default IndexPage
->>>>>>> pages-homeheader-banner-info-sections
