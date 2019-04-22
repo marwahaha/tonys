01f5c53b38a38958b5a447b93c1b3f64a2276a28
Clive Noonan <clivenoonan93@gmail.com>
2019-04-22 01:59:18 +0100

    created menu & product hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-
 import React from 'react'
 import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/bcg/homeBcg.jpeg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
 import Gallery from '../components/HomePageComponents/Gallery'
+import Menu from '../components/HomePageComponents/Menu'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -19,6 +18,7 @@ const IndexPage = () => (
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Menu />
     <Gallery />
   </Layout>
 )
